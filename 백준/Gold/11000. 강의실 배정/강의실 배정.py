724caa8ba2ca4787dd06aee3d408ffb2f5845319
Owon <chlehdnjs@gmail.com>
2023-11-06 14:59:53 +0900

    [Gold V] Title: 강의실 배정, Time: 400 ms, Memory: 73448 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+input = sys.stdin.readline
+n = int(input())
+arr = [list(map(int,input().split())) for _ in range(n)]
+arr.sort()
+q = []
+heapq.heappush(q,arr[0][1])
+for i in range(1,n):
+    if q[0] > arr[i][0]:
+        heapq.heappush(q,arr[i][1])
+    else:
+        heapq.heappop(q)
+        heapq.heappush(q,arr[i][1])
+print(len(q))
