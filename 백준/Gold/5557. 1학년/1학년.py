2619ef96baf8953e4466a72ae1049354869a03a1
Owon <chlehdnjs@gmail.com>
2023-12-04 00:30:05 +0900

    [Gold V] Title: 1학년, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/5557. 1학년/1학년.py
@@ -0,0 +1,19 @@
+# 시간제한 1초
+import sys
+input = sys.stdin.readline
+
+n= int(input())
+arr = list(map(int,input().split()))
+dp = [[0]*21 for _ in range(n)]
+
+ans = arr.pop()
+dp[0][arr[0]] = 1
+
+for i in range(1,n-1):
+    for j in range(21):
+        if dp[i-1][j]:
+            if j-arr[i] >= 0:
+                dp[i][j-arr[i]] += dp[i-1][j]
+            if j+arr[i] <= 20:
+                dp[i][j+arr[i]] += dp[i-1][j]
+print(dp[n-2][ans])
