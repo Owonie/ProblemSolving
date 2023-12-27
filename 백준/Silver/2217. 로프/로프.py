9f39cc1a332c5b4d0a0b00e6ae6e91ecc20bf271
Owon <chlehdnjs@gmail.com>
2023-12-27 09:25:45 +0900

    [Silver IV] Title: 로프, Time: 3940 ms, Memory: 39268 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+arr = []
+ans = []
+
+for _ in range(n):
+    arr.append(int(input()))
+arr.sort(reverse=True)
+
+for i in range(n):
+    ans.append(arr[i]*(i+1))
+print(max(ans))
