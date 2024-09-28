3d33c06542e9a4c5de4801d0421a3b5cf253cc95
Owon <chlehdnjs@gmail.com>
2024-09-28 22:16:53 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,7 +1,7 @@
 function solution(s) {
     var answer = '';
     
-    const number = s.split(" ").map(number => Number(number));
+    const number = s.split(" ");
     ansmin = Math.min(...number);
     ansmax = Math.max(...number);
     return ansmin+' '+ansmax;
